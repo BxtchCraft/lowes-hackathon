88bcaaaa01e60e4bd3f5525fc8c1001e876571b9
Lindsay Zierk <lindsayzierk@pm.me>
2023-06-07 15:44:16 -0500

    Added SCSS file structure

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../scss/components/Header.scss';
+
+function Header() {
+    return (
+        <header>
+            <p>Hello world!</p>
+        </header>
+    );
+}
+
+export default Header;
