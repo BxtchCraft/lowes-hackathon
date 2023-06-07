c59e2a0ec4be09f67eccb8f9d0e374d2e1e11474
Lindsay Zierk <lindsayzierk@pm.me>
2023-06-07 17:04:14 -0500

    Added Font Awesome for React

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
 import '../scss/components/Header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+
+
+
 
 function Header() {
     return (
         <header>
-            <p>Hello world!</p>
+            <FontAwesomeIcon icon={icon({name: 'bars', family: 'sharp', style: 'solid'})} size="lg" />
+            <img class="logo" src="/images/lowes-pro-logo.svg" />
+            <FontAwesomeIcon icon={icon({name: 'circle-user', style: 'regular'})} size="lg" />
+            <FontAwesomeIcon icon={icon({name: 'cart-shopping', style: 'regular', })} size="lg" />
         </header>
     );
 }
